fa7d37ea967108626b53048bdbd7ee0631fd9989
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2026-08-16 20:42:09 +0200

    Environment Setup and Configuration
    - installed dotenv
    - created .env file
    - updated index.js

--- secure-mern-app/api/index.js
+++ secure-mern-app/api/index.js
@@ -1,28 +1,33 @@
+// Load environment variables
+require('dotenv').config(); 
 
 // Import Express
 const express = require('express'); 
 const app = express(); 
+
+//Use the environment variables
+const PORT = process.env.PORT; 
 
 // Enable JSON request handling
 app.use(express.json()); 
 
 // Add a root route
 app.get('/', (req, res) => { 
-res.send('API is running'); 
-});
+  res.send(process.env.APP_NAME); 
+}); 
 
 //Add a health route
 app.get('/health', (req, res) => { 
-res.json({ status: 'OK' }); 
+    res.json({ status: 'OK' }); 
 }); 
 
 // Add a POST route
 app.post('/message', (req, res) => { 
-const { message } = req.body; 
-res.json({ received: message }); 
+    const { message } = req.body; 
+    res.json({ received: message }); 
 }); 
 
 //Start the server
-app.listen(3000, () => { 
-console.log('Server running on port 3000'); 
+app.listen(PORT, () => { 
+  console.log(`Running on port ${PORT}`); 
 }); 
